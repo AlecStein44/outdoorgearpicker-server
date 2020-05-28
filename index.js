7dd939c9cf4b6689c9b40012e4ea2c9e9b9f1da0
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2020-05-27 20:11:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ app.get('/ammo', (req, res) => {
       .where({type: 'bipod'})
       .then(typeData => {
           if (typeData.length === 0) {
-            request({url: `${url}/Mediapartners/IRSYkqTyNep22276244pB9TuBUoBytYTN1/Catalogs/ItemSearch?Query=Category='SHOOTING/GUN ACCESSORIES'`, headers: headers}, function (error, response, body) {
+            request({url: `${url}/Mediapartners/IRSYkqTyNep22276244pB9TuBUoBytYTN1/Catalogs/ItemSearch?Query=Labels='Bipods'`, headers: headers}, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     let newBody = JSON.parse(body)
                     let newItems = []
